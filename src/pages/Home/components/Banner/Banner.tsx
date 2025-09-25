7d598f31e536ea9740f8e7803c82bde93f36edb2
José <jose@arnaldo.dev>
2025-09-25 16:34:08 -0300

    fix: navbar logo and position

--- src/pages/Home/components/Banner/Banner.tsx
+++ src/pages/Home/components/Banner/Banner.tsx
@@ -1,29 +1,17 @@
 import styles from './Banner.module.css'
 import tec from "../../../../assets/tec.png"
-('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
-
-
-
 
 function Banner() {
   return (
-      
       <div className={styles.banner}>
-          <div className={styles.titulobanner}>
-               <p className={styles.subtitulo}>DO DIA 00 À 09 DE OUTUBRO -  SISWEEK</p>
-
-               <h1 className={styles.titulo}>SEMANA DA INOVAÇÃO E <br></br> <span
-                className={styles.destaque}>TECNOLOGIA</span>  
-               </h1>
-
-          
-               
-              
-          </div>
-           <img className={styles.tec} src={tec} alt="tec"/>
+        <div className={styles.titulobanner}>
+          <p className={styles.subtitulo}>DO DIA 00 À 09 DE OUTUBRO - SISWEEK</p>
+          <h1 className={styles.titulo}>SEMANA DA INOVAÇÃO E <br></br> <span
+              className={styles.destaque}>TECNOLOGIA</span>
+          </h1>
+        </div>
+        <img className={styles.tec} src={tec} alt="tec"/>
       </div>
-          
-            
   )
 }
 
